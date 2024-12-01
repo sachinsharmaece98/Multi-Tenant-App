3fb30921c48e1ee9ec70337d6947b49c75c6e81d
Sachin Sharma <sachin.wiz91@gmail.com>
2024-12-01 23:08:42 +0530

    feat: configure settings.py for multi-tenancy, add adminapp and tenantapp, and define tenant schema in adminapp models.py

---
+++ tenantproject/adminapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django_tenants.models import TenantMixin, DomainMixin
+
+# Create your models here.
+
+class Tenant(TenantMixin):
+    name = models.CharField(max_length=100)
+    created_on = models.DateField(auto_now_add = True)
+
+class Domain(DomainMixin):
+    pass
+
